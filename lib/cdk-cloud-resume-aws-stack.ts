4e565a3aa35c05a67d7982d7918398242ce1965e
Andre Burke <aburke.dev@gmail.com>
2022-07-15 22:16:30 -0600

    created s3 bucket and deployment

--- lib/cdk-cloud-resume-aws-stack.ts
+++ lib/cdk-cloud-resume-aws-stack.ts
@@ -1,4 +1,6 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +8,16 @@ export class CdkCloudResumeAwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'CloudResumeBucket', {
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCloudResumeAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const deployment = new BucketDeployment(this, 'CloudResumeBucketDeployment', {
+      sources: [Source.asset('./websites')],
+      destinationBucket: bucket,
+    });
   }
 }
