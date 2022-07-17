8ffd5bb2693d9afd0928621a6cc41c6720da20ec
Andre Burke <aburke.dev@gmail.com>
2022-07-16 19:20:01 -0600

    added commented api gateway

--- lib/cdk-cloud-resume-aws-stack.ts
+++ lib/cdk-cloud-resume-aws-stack.ts
@@ -1,22 +1,25 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class CdkCloudResumeAwsStack extends Stack {
-	constructor(scope: Construct, id: string, props?: StackProps) {
-		super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-		const bucket = new Bucket(this, 'CloudResumeBucket', {
-			publicReadAccess: true,
-			removalPolicy: RemovalPolicy.DESTROY,
-			websiteIndexDocument: 'index.html',
-			autoDeleteObjects: true,
-		});
+    const bucket = new Bucket(this, 'CloudResumeBucket', {
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+      autoDeleteObjects: true,
+    });
 
-		new BucketDeployment(this, 'CloudResumeBucketDeployment', {
-			sources: [Source.asset('./websites')],
-			destinationBucket: bucket,
-		});
-	}
+    new BucketDeployment(this, 'CloudResumeBucketDeployment', {
+      sources: [Source.asset('./websites')],
+      destinationBucket: bucket,
+    });
+
+    // new LambdaRestApi(this, 'CloudResumeApi', {});
+  }
 }
