d861c4facd72319ba552ee0936f0bb08d767c343
Andre Burke <aburke.dev@gmail.com>
2022-07-19 00:39:53 -0600

    added lambda function and api gateway

--- lib/cdk-cloud-resume-aws-stack.ts
+++ lib/cdk-cloud-resume-aws-stack.ts
@@ -1,5 +1,7 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
@@ -20,6 +22,14 @@ export class CdkCloudResumeAwsStack extends Stack {
       destinationBucket: bucket,
     });
 
-    // new LambdaRestApi(this, 'CloudResumeApi', {});
+    const handler = new NodejsFunction(this, 'CloudResumeHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      memorySize: 256,
+      timeout: Duration.minutes(2),
+    });
+
+    new LambdaRestApi(this, 'CloudResumeApi', {
+      handler: handler,
+    });
   }
 }
