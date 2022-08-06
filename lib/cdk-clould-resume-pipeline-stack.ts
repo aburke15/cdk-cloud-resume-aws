6944076a4d0b217028e3b835491df20f5c612d8c
Andre Burke <aburke.dev@gmail.com>
2022-08-06 16:33:19 -0600

    added some string templates

--- lib/cdk-clould-resume-pipeline-stack.ts
+++ lib/cdk-clould-resume-pipeline-stack.ts
@@ -1,8 +1,18 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCloudResumePipelineStack extends Stack {
+  private readonly cloudResumePipeline: string = 'CloudResumePipeline';
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const cloudResumePipeline: CodePipeline = new CodePipeline(this, this.cloudResumePipeline, {
+      pipelineName: this.cloudResumePipeline,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('aburke15/cdk-cloud-resume-aws', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
   }
 }
